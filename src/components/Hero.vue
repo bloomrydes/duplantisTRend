be3459af1c6e360133161c0effe69d86bb703cd2
onyillto <56700691+onyillto@users.noreply.github.com>
2024-10-21 21:09:45 +0100

    push

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -13,7 +13,7 @@
       <div class="text-center z-10"> <!-- Ensure text is above the video -->
         <h1 class="text-3xl font-semibold text-white lg:text-4xl">
            
-          <span class="text-orange-400">Duplantis Trends:</span> <span class="text-blue-900">Discovering the trends shaping our world</span>
+          <span class="text-orange-400">Duplantis Trends:</span> <span class="text-blue-900">Discovering the trends shaping our world...</span>
         </h1>
         <router-link to="/products">
           <button class="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-[#1f2937] rounded-md lg:w-auto hover:bg-blue-900 focus:outline-none focus:bg-[#083344] md:text-base md:px-4 md:py-3">
