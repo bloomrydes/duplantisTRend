1d4e7d7fe9e37fd2a6d293822d095ea98abd4cd1
onyillto <56700691+onyillto@users.noreply.github.com>
2024-11-12 16:51:18 +0100

    pushg

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,21 +9,18 @@
       <source src="/video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
-    <div class="flex flex-col items-center justify-center w-full h-full bg-black/60"> <!-- Darker overlay for visibility -->
-      <div class="text-center z-10"> <!-- Ensure text is above the video -->
-        <h1 class="text-3xl font-semibold text-white lg:text-4xl">
+    <div class="flex flex-col items-center justify-center w-full h-full bg-black/60">
+      <div class="text-center z-10 max-w-3xl px-4"> <!-- Restrict max width and add padding -->
+        <h1 class="text-3xl font-semibold text-white lg:text-6xl">
           <span class="text-white">Duplantis Trends:</span>
           <span class="text-orange-400"> Discovering the trends shaping our world...</span>
         </h1>
       </div>
-      <router-link to="/products" class="mt-4"> <!-- Added margin to separate button from text -->
-        <button class="w-full px-5 py-2 sm:w-[30%] text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-[#1f2937] rounded-md lg:w-auto md:w-1/2 hover:bg-blue-900 focus:outline-none focus:bg-[#083344] md:text-base md:px-4 md:py-3">
-          Buy Now
-        </button>
-      </router-link>
+    
     </div>
   </header>
 </template>
+
 
 <script>
 export default {
