3ec2f03e20e15861cac48e72791a6bb447a174d1
onyillto <56700691+onyillto@users.noreply.github.com>
2024-12-21 18:00:42 +0100

    push

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,1 +1,19 @@
-zfv
+
+<template>
+ 
+<Header/>
+<BlogContent/>
+</template>
+
+<script>
+import Header from '../components/BlogHeading.vue';
+import BlogContent from '../components/BlogContent.vue';
+export default {
+  components: {
+    Header,
+    BlogContent
+  },
+};
+</script>
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,28 @@
-bdgbb
+
+
+<template>
+ <Hero/>
+
+    <BlogComponent/>
+    <NewsletterSection/>
+    <Testimonials/>
+
+</template>
+<script>
+  import Hero from '../components/Hero.vue';
+// import ProductComponent from  '../components/ProductLine.vue';
+import BlogComponent from  '../components/Blog.vue';
+import NewsletterSection from  '../components/NewsLetter.vue';
+import Testimonials from '../components/Testimonial.vue';
+
+export default {
+  components: {
+    Hero,
+    // ProductComponent,
+    BlogComponent,
+    NewsletterSection,
+    Testimonials,
+
+  }
+}
+</script>
